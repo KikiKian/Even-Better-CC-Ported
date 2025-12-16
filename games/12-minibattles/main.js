08ca0c3c393a6e5d459f15ee1716e669952aa8a3
zzzyyyx <aaron.lu.marin@gmail.com>
2025-12-15 16:19:05 -0800

    Add basic text drawing to main.js

---
+++ games/12-minibattles/main.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+/* basic text draw */
+ctx.fillStyle = "white";
+ctx.font = "30px Arial";
+ctx.fillText("12 Mini Battles", 250, 220);
