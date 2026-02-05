e905eccdc898cd15dba875e8fa03c694728cbe9f
Klinoxxx <156397066+KikiKian@users.noreply.github.com>
2026-02-04 22:25:03 -0800

    Add Slope and Pokemon cards, add slope files, firebase config

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+    apiKey: "AIzaSyClOeUmq_Dt-9ghRV4LF-HaAO7JRypNIWw",
+    authDomain: "even-better-cc-ported.firebaseapp.com",
+    projectId: "even-better-cc-ported",
+    storageBucket: "even-better-cc-ported.firebasestorage.app",
+    messagingSenderId: "662470608606",
+    appId: "1:662470608606:web:0b8bfbf38f0de6c56ea44f",
+    measurementId: "G-HYYR7S4BK7"
+};
